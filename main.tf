bddf42e741f9fb37a2d31e1908ca7d91e31f5669
Abuzzi-Geo <abrahamgeorge2810@gmail.com>
2026-04-23 12:27:13 +0530

    Add random_id resource and update S3 bucket name

--- main.tf
+++ main.tf
@@ -1,18 +1,25 @@
 terraform {
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = "~> 5.0"
-}
-}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
 }
 
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
+}
+
+resource "random_id" "bucket_id" {
+  byte_length = 4
 }
 
 resource "aws_s3_bucket" "demo_bucket" {
-bucket = "Abraham-terraform-demo-28102002"
+  bucket = "abraham-terraform-demo-${random_id.bucket_id.hex}"
 }
 
 data "aws_vpc" "default" {
@@ -20,11 +27,12 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_subnet" "public_subnet" {
-vpc_id = data.aws_vpc.default.id
-cidr_block = "10.0.1.0/24"
+  vpc_id     = data.aws_vpc.default.id
+  cidr_block = "172.31.1.0/24"
 }
 
 resource "aws_instance" "ec2" {
-ami = "ami-0f5ee92e2d63afc18"
-instance_type = "t2.micro"
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
 }
